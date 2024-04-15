3683884e3950c28da52d395da7997f74c4ad27d3
Part <pkhillan1@gmail.com>
2024-04-15 00:42:29 -0500

    Connected server to MongoDB

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 const port = 3000
 
@@ -6,6 +7,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/blog', (req, res) => {
+  res.send('Hello Blog, My name is Parth.')
 })
+
+
+mongoose.connect("mongodb+srv://pkhillan:Khillanji416!@cluster0.8szw8tv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => {
+  console.log('Connected to MongoDB')
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+}).catch(() => {
+  console.log('error')
+});
+
