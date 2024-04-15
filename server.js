a0f7374f85f41b9d6580a394c495e1475757c45e
Part <pkhillan1@gmail.com>
2024-04-15 08:38:25 -0500

    Created basic structure of API

--- server.js
+++ server.js
@@ -1,24 +1,45 @@
-const express = require('express')
-const mongoose = require('mongoose');
-const app = express()
-const port = 3000
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Read the data.json file into memory
+const dataFilePath = './data.json'; // Ensure the path is correct relative to server.js
+let data = {};
 
-app.get('/blog', (req, res) => {
-  res.send('Hello Blog, My name is Parth.')
-})
-
-
-mongoose.connect("mongodb+srv://pkhillan:Khillanji416!@cluster0.8szw8tv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => {
-  console.log('Connected to MongoDB')
-
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
-}).catch(() => {
-  console.log('error')
+fs.readFile(dataFilePath, (err, fileData) => {
+    if (err) {
+        console.error("Failed to read data.json file:", err);
+        return;
+    }
+    data = JSON.parse(fileData.toString());
 });
 
+// Endpoint to get all exercises
+app.get('/api/exercises', (req, res) => {
+    res.json(data.exercises || []);
+});
+
+// Endpoint to get all body parts
+app.get('/api/bodyparts', (req, res) => {
+    res.json(data.bodyParts || []);
+});
+
+// Endpoint to get details for a specific exercise
+app.get('/api/exercise/detail/:id', (req, res) => {
+    const exerciseDetail = data.exerciseDetails[req.params.id];
+    if (exerciseDetail) {
+        res.json(exerciseDetail);
+    } else {
+        res.status(404).send('Exercise detail not found');
+    }
+});
+
+// Endpoint to serve the complete data.json for other miscellaneous needs
+app.get('/api/data', (req, res) => {
+    res.json(data);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
