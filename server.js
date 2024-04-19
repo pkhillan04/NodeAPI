f2b1c91618cb01e4516a22392474af8840d0ede9
Part <pkhillan1@gmail.com>
2024-04-19 02:31:27 -0500

    Added cors to the project

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const fs = require('fs');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 const app = express();
 dotenv.config({ path: "./config.env" });
-
 app.use(express.json());
+app.use(cors());
 
-// Read the data.json file into memory
-const dataFilePath = './data.json'; // Ensure the path is correct relative to server.js
+const dataFilePath = './data.json';
 let data = [];
 
 fs.readFile(dataFilePath, (err, fileData) => {
@@ -19,46 +19,39 @@ fs.readFile(dataFilePath, (err, fileData) => {
     data = JSON.parse(fileData.toString());
 });
 
-// Endpoint to get exercises by body part
 app.get('/api/exercises/bodyPart/:bodyPart', (req, res) => {
     const bodyPart = req.params.bodyPart.toLowerCase();
     const exercisesByBodyPart = data.filter(exercise => exercise.bodyPart.toLowerCase() === bodyPart);
     res.json(exercisesByBodyPart);
 });
 
-// Endpoint to get the list of body parts
 app.get('/api/exercises/bodyPartList', (req, res) => {
     const bodyPartsList = Array.from(new Set(data.map(exercise => exercise.bodyPart)));
     res.json(bodyPartsList);
 });
 
-// Endpoint to get the list of equipment
 app.get('/api/exercises/equipmentList', (req, res) => {
     const equipmentList = Array.from(new Set(data.map(exercise => exercise.equipment)));
     res.json(equipmentList);
 });
 
-// Endpoint to get the list of target muscles
 app.get('/api/exercises/targetList', (req, res) => {
     const targetList = Array.from(new Set(data.map(exercise => exercise.target)));
     res.json(targetList);
 });
 
-// Endpoint to get exercises by equipment type
 app.get('/api/exercises/equipment/:type', (req, res) => {
     const equipmentType = req.params.type.toLowerCase();
     const exercisesByEquipment = data.filter(exercise => exercise.equipment.toLowerCase() === equipmentType);
     res.json(exercisesByEquipment);
 });
 
-// Endpoint to get exercises by target muscle
 app.get('/api/exercises/target/:target', (req, res) => {
     const targetMuscle = req.params.target.toLowerCase();
     const exercisesByTarget = data.filter(exercise => exercise.target.toLowerCase() === targetMuscle);
     res.json(exercisesByTarget);
 });
 
-// Endpoint to get exercise by ID
 app.get('/api/exercises/exercise/:id', (req, res) => {
     const exerciseId = req.params.id;
     const exercise = data.find(exercise => exercise.id === exerciseId);
@@ -69,7 +62,6 @@ app.get('/api/exercises/exercise/:id', (req, res) => {
     }
 });
 
-// Endpoint to get exercise by name
 app.get('/api/exercises/name/:name', (req, res) => {
     const exerciseName = req.params.name.toLowerCase();
     const exercise = data.find(exercise => exercise.name.toLowerCase() === exerciseName);
@@ -80,13 +72,11 @@ app.get('/api/exercises/name/:name', (req, res) => {
     }
 });
 
-// Endpoint to get all exercises
 app.get('/api/exercises', (req, res) => {
     res.json(data);
 });
 
-// Start the server
 app.listen(process.env.PORT, () => {
-  console.log(`Server running on http://localhost:${process.env.PORT}`);
-  console.log(`Frontend: ${process.env.FRONTEND_URL}`);
+    console.log(`Server running on http://localhost:${process.env.PORT}`);
+    console.log(`Frontend: ${process.env.FRONTEND_URL}`);
 });
